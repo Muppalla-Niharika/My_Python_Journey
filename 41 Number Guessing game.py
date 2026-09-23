ffdc426ccc4a75d8b4a4df1d30eb4b2f26fd3c0e
Niharika <niharikamuppalla3@gmail.com>
2026-09-23 22:17:15 +0530

    Added Number Guessing Game with exception handling

--- 41 Number Guessing game.py
+++ 41 Number Guessing game.py
@@ -1,6 +1,6 @@
 import random
 print("Welcome to Number Guessing Game!")
-print("Choose difficulty:")
+print("Choose difficulty:") #spitting them into 3 levels 
 print("1 — Easy (1 to 50)")
 print("2 — Medium (1 to 100)")
 print("3 — Hard (1 to 200)")
@@ -8,16 +8,19 @@ level = input("Enter 1, 2 or 3: ")
 if level == "1":
     secret_number = random.randint(1, 50)
     max_num = 50
+    max_attempts = 10
 elif level == "2":
     secret_number = random.randint(1, 100)
     max_num = 100
+    max_attempts = 7
 else:
     secret_number = random.randint(1, 200)
     max_num = 200
+    max_attempts = 5
 guess = 0
 attempts = 0
 print(f"I am thinking of a number between 1 and {max_num}!")
-while guess != secret_number:
+while guess != secret_number and attempts < max_attempts:
     try:
         guess = int(input(f"Guess the number between 1 and {max_num}: "))
         attempts += 1
@@ -29,3 +32,5 @@ while guess != secret_number:
             print("Too Low!! Try Again !")
     except ValueError:
         print("Please Enter a valid number! ")
+if guess != secret_number:
+    print(f"Game Over! The number was {secret_number}!")
